00ed427cf9809a3e677bd9584fab9c6238ba3937
Harold Asai <koichiroasai@gmail.com>
2017-07-06 15:33:26 -0700

    Create from command completed-r1

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ActiveRecord::Base
 	validates :name, presence: true
 	validates :email, presence: true
 	validates :password_digest, presence: true
-	validates :password, presence: true
 end

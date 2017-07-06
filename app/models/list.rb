005850a1d568235e27e697ffcc12ab075d4f14be
Harold Asai <koichiroasai@gmail.com>
2017-07-06 15:23:47 -0700

    Create from command completed

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,7 @@
 class List < ActiveRecord::Base
 	belongs_to :user
 	has_many :items, dependent: :destroy
+
+	validates :name, presence: true
+	validates :private, exclusion: { in: [nil] }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
 	has_many :lists, dependent: :destroy
+
+	validates :name, presence: true
+	validates :email, presence: true
+	validates :password_digest, presence: true
+	validates :password, presence: true
 end
